d464f56cef1045a46c9c8efb1b31228b930c2efe
chris_rex <christr@thoughtworks.com>
2013-03-21 15:59:32 +0530

    [Rex] Automated e2e scenario of "Upgrade AMP client" using Send App feature

---
+++ android-server/src/main/java/com/uiautomator_cucumber/android_server/actions/SelectFromAppsList.java
@@ -0,0 +1,24 @@
+package com.uiautomator_cucumber.android_server.actions;
+
+import com.android.uiautomator.core.*;
+import com.uiautomator_cucumber.android_server.Action;
+import com.uiautomator_cucumber.android_server.Result;
+
+import java.util.Map;
+
+public class SelectFromAppsList extends Action {
+    public SelectFromAppsList(UiDevice uiDevice) {
+        super(uiDevice);
+    }
+
+    @Override
+    public Result execute(Map<String, Object> arguments) throws UiObjectNotFoundException {
+        String appName = (String) arguments.get("appName");
+        //TODO: Using a better selector
+        UiScrollable settingsMenu = new UiScrollable(new UiSelector().scrollable(true).focused(false));
+        settingsMenu.setAsVerticalList();
+
+        (settingsMenu.getChildByText(new UiSelector().className(android.widget.TextView.class.getName()),appName)).click();
+        return Result.OK;
+    }
+}
