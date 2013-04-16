41fc6debbea1f1fb50a353e035c1fd10fa8c3ddd
John Barker <jbarker@amplify.com>
2013-04-16 16:49:56 -0400

    BETA-1539 [JB] Allow use of default device in DSL

--- lib/honeydew/dsl.rb
+++ lib/honeydew/dsl.rb
@@ -7,5 +7,11 @@ module Honeydew
         end
       EOF
     end
+
+    def use_default_device(name)
+      define_singleton_method name.to_sym do |*args, &block|
+        Honeydew.using_device(Honeydew.default_device_serial, &block)
+      end
+    end
   end
 end
