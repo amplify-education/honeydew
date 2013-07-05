ea1f1bd5abd83e9294f5ebd9db69e683ef1bcf95
John Barker <jbarker@amplify.com>
2013-07-05 09:15:14 -0400

    Use the first device if no serial specified

--- lib/honeydew/dsl.rb
+++ lib/honeydew/dsl.rb
@@ -1,6 +1,6 @@
 module Honeydew
   module DSL
-    def use_device(name, serial)
+    def use_device(name, serial = nil)
       define_singleton_method name.to_sym do |*args, &block|
         Honeydew.using_device(serial, &block)
       end
